a9b72038aa379eb476630ac208c1493d94563ce1
Bong_gamja <whynot008100@gmail.com>
2025-12-10 18:34:03 +0900

    에러수정중

--- app/layout.tsx
+++ app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         >
           <SyncUserProvider>
             <Navbar />
-            {children}
+          {children}
           </SyncUserProvider>
         </body>
       </html>
